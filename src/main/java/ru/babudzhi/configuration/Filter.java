d4f2ecacae4c4ddb8f9599f94ff3b79e9fa18dab
babudzhi <babudhi@lanit.ru>
2018-06-08 18:11:27 +0300

    refactoring1

---
+++ src/main/java/ru/babudzhi/configuration/Filter.java
@@ -0,0 +1,25 @@
+package ru.babudzhi.configuration;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class Filter implements javax.servlet.Filter {
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain next) throws IOException, ServletException {
+            request.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html; charset=UTF-8");
+            response.setCharacterEncoding("UTF-8");
+            next.doFilter(request, response);
+        }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+}
